7406aec6f246aed8ee4c2768574b45c9448ade77
arash js <arash.javanshour1993@gmail.com>
2022-04-13 14:30:33 +0200

    init cra

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,7 @@ export default function Header(){
     return(
         <header className="header">
             <img className="header-logo" src={airbnb}/>
+            <h1>Hello</h1>
         </header>
     );
 }
