f715991214ce70ac476d77bc46a21d2fe9106872
Arash Javanshour <41967401+Arashjs1993@users.noreply.github.com>
2022-04-15 14:24:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,9 @@ import Card from "./Components/Card";
 import Data from "./Data";
 
 function App() {
-  /*Map function of the Data array
-  turns each data object in the Data.js into 
-  the representable JSX format on the browser, 
-  inside the map function there is loop that
-  loops through all the data objects in Data.js.
-  The input of the map func is the function
-  that we want to apply to each of the items the loop
-  of the map goes over them*/
   const cardsArray = Data.map(hero => {
     return <Card 
       key = {hero.id}
-      /*Instead of passing the object props
-      one by one we can pass the entire hero object
-      to the Card component, in this way we
-      reduce the coding effort a lot */
       item = {hero}
     />
   })
